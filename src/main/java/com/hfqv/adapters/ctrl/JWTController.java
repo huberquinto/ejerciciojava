950b3414d5e69c05a00f0d133b943b3a2d8691a8
huberquinto <huberfqv@gmail.com>
2024-04-03 12:26:04 -0500

    huberquinto se adiciona logs

--- src/main/java/com/hfqv/adapters/ctrl/JWTController.java
+++ src/main/java/com/hfqv/adapters/ctrl/JWTController.java
@@ -3,27 +3,28 @@ package com.hfqv.adapters.ctrl;
 
 import com.hfqv.app.domain.model.TokenRp;
 import com.hfqv.app.domain.util.JWTUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api")
 public class JWTController {
 
+    private static final Logger logger = LoggerFactory.getLogger(JWTController.class);
 
     @Autowired
     private JWTUtil jWTUtil;
 
-    @PostMapping(value = "/token",produces = "application/json")
+    @GetMapping(value = "/token", produces = "application/json")
     public ResponseEntity<Object> getToken(@RequestHeader("UserLogin") String userLogin){
         TokenRp tokenRp = new TokenRp();
         try{
             String token =  jWTUtil.generateToken(userLogin);
+            logger.info("TOKEN GENERADO: "+token);
             tokenRp.setTokenCode(token);
             return ResponseEntity.ok().body(tokenRp);
         }catch(Exception ex){
